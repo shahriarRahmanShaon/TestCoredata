d2ce5d82828f66f24dec204ff1d65189bf7ed4a0
sergio Shaon <shaon463@gmail.com>
2022-04-12 14:16:42 +0600

    Initial Commit

---
+++ TestCoredata/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCoredata
+//
+//  Created by sergio shaon on 12/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestCoredata/TestCoredataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCoredataApp.swift
+//  TestCoredata
+//
+//  Created by sergio shaon on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct TestCoredataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
